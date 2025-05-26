40b409f72aff90e7def074ea7ca30b6615a1ae60
Eric Del Socorro <erickdelsocorro@gmail.com>
2025-05-26 19:00:14 +0800

    grpc communnication between services

---
+++ billing-service/src/test/java/com/may/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
