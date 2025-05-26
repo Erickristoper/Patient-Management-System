dd801a5adc74bca24615ce22868c3f569690c318
Eric Del Socorro <erickdelsocorro@gmail.com>
2025-05-26 11:11:23 +0800

    custom exceptionhandler

--- patient-service/src/main/java/com/may/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/may/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.may.patientservice.service;
 
 import com.may.patientservice.dto.PatientRequestDTO;
 import com.may.patientservice.dto.PatientResponseDTO;
+import com.may.patientservice.exception.EmailAlreadyExistsException;
 import com.may.patientservice.mapper.PatientMapper;
 import com.may.patientservice.model.Patient;
 import com.may.patientservice.repository.PatientRepository;
@@ -23,6 +24,10 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException("A patient with this email: " + patientRequestDTO.getEmail() +
+                    " already exists");
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toPatientModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
     }
