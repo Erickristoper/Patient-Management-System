877efafd355e83883ef69da08a65c60acef31666
Eric Del Socorro <erickdelsocorro@gmail.com>
2025-05-31 21:27:42 +0800

    custom exception handler as restcontrolleradvice

--- auth-service/src/main/java/com/may/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/may/authservice/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.may.authservice.dto.LoginRequestDTO;
 import com.may.authservice.dto.LoginResponseDTO;
 import com.may.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.Optional;
 @RestController
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -38,11 +41,13 @@ public class AuthController {
     @GetMapping("/validate")
     public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader){
         // Authorization: Bearer <token>
+        log.info("Auth header: {}", authHeader);
         if(authHeader == null || !authHeader.startsWith("Bearer ")) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+        log.info("AUthorize: {}", authService.validateToken(authHeader.substring(7)));
         return authService.validateToken(authHeader.substring(7))
                 ? ResponseEntity.ok().build()
-                :ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
